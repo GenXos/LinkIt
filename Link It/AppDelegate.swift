4b1e07dbeb05d09082ba523dc87d9ff77d90387e
Todd Fields <toddwfields@gmail.com>
2017-05-10 16:21:04 -0500

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Todd Fields on 2017-05-10.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Link It
+//
+//  Created by Todd Fields on 2017-05-10.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
